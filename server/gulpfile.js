03dac2ff79c07e5539694dc7522c3a675ff6d367
Atikur Rahman <atikassam@outlook.com>
2018-04-26 03:07:01 +0530

    Server environment set

---
+++ server/gulpfile.js
@@ -0,0 +1,16 @@
+// Require gulp task runner
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('tsconfig.json');
+
+const _BUILD_PATH_ = __dirname+'/dist';
+
+// Now lets create a task which will transpile
+// our typescript files to targeted js file
+
+gulp.task('default', ()=>{
+	return tsProject.src()
+			.pipe(tsProject())
+			.js.pipe(gulp.dest(_BUILD_PATH_));
+})
+
